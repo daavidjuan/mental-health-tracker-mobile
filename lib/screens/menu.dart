5d16f9c674361c5054b1c859531e10682f921dff
daavidjuan <ddavidjuann@gmail.com>
2024-11-13 14:20:44 +0700

    tutorial 8 finish

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/widgets/left_drawer.dart';
-// TODO: Impor halaman MoodEntryFormPage jika sudah dibuat
 import 'package:mental_health_tracker/widgets/mood_card.dart';
 
 class MyHomePage extends StatelessWidget {
